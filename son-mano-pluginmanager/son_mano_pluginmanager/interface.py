1567fc879acf6e3f61d27d4901695c77ce7c7c31
Manuel Peuster <manuel@peuster.de>
2016-03-30 16:49:12 +0200

    Added basic REST endpoint logic

--- son-mano-pluginmanager/son_mano_pluginmanager/interface.py
+++ son-mano-pluginmanager/son_mano_pluginmanager/interface.py
@@ -3,8 +3,11 @@ Adds a REST interface to the plugin manager to control plugins registered to the
 """
 import logging
 import threading
+import json
 from flask import Flask, request
 import flask_restful as fr
+from mongoengine import DoesNotExist
+from son_mano_pluginmanager import model
 
 logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger("son-mano-pluginmanger:interface")
@@ -15,32 +18,57 @@ logging.getLogger("werkzeug").setLevel(logging.WARNING)
 class PluginsEndpoint(fr.Resource):
 
     def get(self):
-        LOG.warning("Not implemented: GET.")
-        return "Not implemented.", 500
+        LOG.debug("GET plugin list")
+        return [p.uuid for p in model.Plugin.objects], 200
 
 
 class PluginEndpoint(fr.Resource):
 
     def get(self, plugin_uuid=None):
-        LOG.warning("Not implemented: GET. Arguments: plugin_uuid=%r" % plugin_uuid)
-        return "Not implemented.", 500
+        LOG.debug("GET plugin info for: %r" % plugin_uuid)
+        try:
+            p = model.Plugin.objects.get(uuid=plugin_uuid).first()
+            return p.to_dict(), 200
+        except DoesNotExist as e:
+            LOG.error("Lookup error: %r" % plugin_uuid)
+            return {}, 404
 
     def delete(self, plugin_uuid=None):
-        LOG.warning("Not implemented: DELETE. Arguments: plugin_uuid=%r" % plugin_uuid)
-        return "Not implemented.", 500
+        LOG.debug("DELETE plugin: %r" % plugin_uuid)
+        try:
+            p = model.Plugin.objects.get(uuid=plugin_uuid).first()
+            # send lifecycle stop event to plugin
+            PM.send_stop_notification(p)
+            # TODO ensure that record is deleted even if plugin does not deregister itself
+            return {}, 200
+        except DoesNotExist as e:
+            LOG.error("Lookup error: %r" % plugin_uuid)
+            return {}, 404
 
 
 class PluginLifecycleEndpoint(fr.Resource):
 
-    def get(self, plugin_uuid=None):
-        LOG.warning("Not implemented: GET. Arguments: plugin_uuid=%r" % plugin_uuid)
-        return "Not implemented.", 500
-
     def put(self, plugin_uuid=None):
-        LOG.warning("Not implemented: PUT. Arguments: plugin_uuid=%r" % plugin_uuid)
-        return "Not implemented.", 500
+        LOG.debug("PUT plugin lifecycle: %r" % plugin_uuid)
+        try:
+            p = model.Plugin.objects.get(uuid=plugin_uuid).first()
+            # get target state from request body
+            ts = json.loads(request.json()).get("target_state")
+            if ts == "start":
+                 PM.send_start_notification(p)
+            elif ts == "pause":
+                PM.send_pause_notification(p)
+            elif ts == "stop":
+                PM.send_stop_notification(p)
+            else:
+                return {"message": "Malformed request"}, 500
+            return {}, 200
+        except DoesNotExist as e:
+            LOG.error("Lookup error: %r" % plugin_uuid)
+            return {}, 404
 
-
+# reference to plugin manager
+PM = None
 # setup Flask
 app = Flask(__name__)
 api = fr.Api(app)
@@ -59,7 +87,9 @@ def _start_flask(host, port):
             )
 
 
-def start(host="0.0.0.0", port=8001):
+def start(pm, host="0.0.0.0", port=8001):
+    global PM
+    PM = pm
     thread = threading.Thread(target=_start_flask, args=(host, port))
     thread.daemon = True
     thread.start()
