f4702f233a5d7ff1d42bfb6fbb0bd08d5853b141
Manuel Peuster <manuel@peuster.de>
2016-04-20 11:50:00 +0200

    changed tests

--- test/disabled_test_plugin-son-mano-slm.sh
+++ test/disabled_test_plugin-son-mano-slm.sh
@@ -19,10 +19,6 @@ trap "docker kill test.broker; docker kill test.mongo; docker kill test.pluginma
 set -e
 
 echo "test_plugin-son-mano-slm.sh"
-# build Docker images
-docker build -t test.pluginmanager -f son-mano-pluginmanager/Dockerfile .
-docker build -t test.slm -f plugins/son-mano-service-lifecycle-management/Dockerfile .
-
 # spin up container with broker (in daemon mode)
 docker run -d -p 5672:5672 --name test.broker rabbitmq:3
 # spin up container with MongoDB (in daemon mode)
@@ -30,11 +26,11 @@ docker run -d -p 27017:27017 --name test.mongo mongo
 # wait a bit for broker startup
 sleep 10
 # spin up the plugin manager
-docker run -d --link test.broker:broker --link test.mongo:mongo --name test.pluginmanager test.pluginmanager
+docker run -d --link test.broker:broker --link test.mongo:mongo --name test.pluginmanager registry.sonata-nfv.eu:5000/pluginmanager
 # wait a bit for manager startup
 sleep 3
 # spin up slm container and run py.test
-docker run --link test.broker:broker --name test.slm test.slm py.test -v
+docker run --link test.broker:broker --name test.slm registry.sonata-nfv.eu:5000/servicelifecyclemanagement py.test -v
 
 
 echo "done."
