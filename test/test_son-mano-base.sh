f42ef41422e76599a1747b3eb0d94a40b01633d6
Manuel Peuster <manuel@peuster.de>
2016-04-20 11:47:10 +0200

    test change

--- test/test_son-mano-base.sh
+++ test/test_son-mano-base.sh
@@ -7,7 +7,7 @@
 # - RabbitMQ
 # - son-mano-base/Dockerfile
 #
-# It triggers the unittest execution in son-mano-base/Dockerfile.
+# It triggers the unittest execution in son-mano-base.
 #
 
 # setup cleanup mechanism
@@ -17,13 +17,11 @@ trap "docker kill test.broker; docker rm test.broker; docker rm test.sonmanobase
 set -e
 
 echo "test_son-mano-base.sh"
-# build Docker images
-#docker build -t test.sonmanobase -f son-mano-base/Dockerfile .
 # spin up container with broker (in daemon mode)
 docker run -d -p 5672:5672 --name test.broker rabbitmq:3
 # wait a bit for broker startup
 sleep 10
 # spin up the son-mano-base test container and execute its unittests
-docker run --link test.broker:broker --name test.sonmanobase sonmanobase py.test -v
+docker run --link test.broker:broker --name test.sonmanobase registry.sonata-nfv.eu:5000/sonmanobase py.test -v
 
 echo "done."
