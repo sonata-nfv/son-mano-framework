99a20c207e1a4a7399eeaf5a3c4d5de885d47adc
Manuel Peuster <manuel@peuster.de>
2016-04-20 15:16:11 +0200

    jenkins fix

--- test/test_son-mano-pluginmanager.sh
+++ test/test_son-mano-pluginmanager.sh
@@ -14,9 +14,10 @@
 # setup cleanup mechanism
 trap "docker kill test.broker; docker kill test.mongo; docker rm test.broker; docker rm test.mongo; docker rm test.pluginmanager" INT TERM EXIT
 
+docker rm -f int-gk-mano.broker
 docker rm -f test.broker
 docker rm -f test.mongo
-docker rm -f tet.pluginmanager
+docker rm -f tset.pluginmanager
 
 #  always abort if an error occurs
 set -e
