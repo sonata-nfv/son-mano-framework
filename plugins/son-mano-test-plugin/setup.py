1236d3e5c2c7445b5e39f32939e3a06121ed542f
Manuel Peuster <manuel@peuster.de>
2016-07-14 12:35:03 +0200

    Cleanup: Cahnged setup.py versions to 0.9

--- plugins/son-mano-test-plugin/setup.py
+++ plugins/son-mano-test-plugin/setup.py
@@ -45,7 +45,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',
+    version='0.9',
 
     description='SONATA test plugin',
     long_description=long_description,
