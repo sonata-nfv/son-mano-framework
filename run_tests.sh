b23097034277dd478f0062ee5f9cde55be564555
Manuel Peuster <manuel@peuster.de>
2016-07-14 11:29:25 +0200

    Cleanup: Deactivated plugins that are not part of the release

--- run_tests.sh
+++ run_tests.sh
@@ -37,10 +37,10 @@ set -e
 # We have to build the containers locally (done by Jenkins job in CI)
 docker build -t registry.sonata-nfv.eu:5000/sonmanobase -f son-mano-base/Dockerfile .
 docker build -t registry.sonata-nfv.eu:5000/pluginmanager -f son-mano-pluginmanager/Dockerfile .
-docker build -t registry.sonata-nfv.eu:5000/exampleplugin -f plugins/son-mano-example-plugin-1/Dockerfile .
+#docker build -t registry.sonata-nfv.eu:5000/exampleplugin -f plugins/son-mano-example-plugin-1/Dockerfile .
 docker build -t registry.sonata-nfv.eu:5000/testplugin -f plugins/son-mano-test-plugin/Dockerfile .
 docker build -t registry.sonata-nfv.eu:5000/servicelifecyclemanagement -f plugins/son-mano-service-lifecycle-management/Dockerfile .
-docker build -t registry.sonata-nfv.eu:5000/functionlifecyclemanagement -f plugins/son-mano-function-lifecycle-management/Dockerfile .
-docker build -t registry.sonata-nfv.eu:5000/placementexecutive -f plugins/son-mano-placement/Dockerfile .
+#docker build -t registry.sonata-nfv.eu:5000/functionlifecyclemanagement -f plugins/son-mano-function-lifecycle-management/Dockerfile .
+#docker build -t registry.sonata-nfv.eu:5000/placementexecutive -f plugins/son-mano-placement/Dockerfile .
 
 for i in `find . -name test_*.sh -type f`; do $i; if [ $? -ne 0 ]; then exit 1; fi; done
