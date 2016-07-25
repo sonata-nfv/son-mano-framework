013fc62b6f9b9259e54406948ee15e78e3a999cf
Hadi <hadi_ksr_2004@yahoo.com>
2016-07-25 13:42:05 +0200

    add SMR

--- test/test_plugin-son-mano-smr.sh
+++ test/test_plugin-son-mano-smr.sh
@@ -39,14 +39,14 @@
 #
 
 # setup cleanup mechanism
-trap "set +e; docker rm -fv broker; docker rm -fv mongo; docker rm -fv pluginmanager; docker rm -fv smr; docker rm -fv ssm1; docker rm -fv ssm2" INT TERM EXIT
+trap "set +e; docker rm -fv test.broker; docker rm -fv test.mongo; docker rm -fv test.pluginmanager; docker rm -fv test.smr; docker rm -fv ssm1; docker rm -fv ssm2" INT TERM EXIT
 
 # ensure cleanup
 set +e
-docker rm -fv broker
-docker rm -fv mongo
-docker rm -fv pluginmanager
-docker rm -fv smr
+docker rm -fv test.broker
+docker rm -fv test.mongo
+docker rm -fv test.pluginmanager
+docker rm -fv test.smr
 docker rm -fv ssm1
 docker rm -fv ssm2
 
@@ -55,23 +55,23 @@ set -e
 
 echo "test_plugin-son-mano-smr.sh"
 # spin up container with broker (in daemon mode)
-docker run -d -p 5672:5672 --name broker rabbitmq:3
+docker run -d -p 5672:5672 --name test.broker rabbitmq:3
 # wait a bit for broker startup
 while ! nc -z localhost 5672; do
 sleep 1 && echo -n .; # waiting for rabbitmq
 done;
 # spin up container with MongoDB (in daemon mode)
-docker run -d -p 27017:27017 --name mongo mongo
+docker run -d -p 27017:27017 --name test.mongo mongo
 # wait a bit for db startup
 while ! nc -z localhost 27017; do
 sleep 1 && echo -n .; # waiting for mongo
 done;
 sleep 3
 # spin up the plugin manager
-docker run -d --link broker:broker --link mongo:mongo --name pluginmanager registry.sonata-nfv.eu:5000/pluginmanager
+docker run -d --link test.broker:broker --link test.mongo:mongo --name pluginmanager registry.sonata-nfv.eu:5000/pluginmanager
 # wait a bit for manager startup
 sleep 3
 # spin up smr container and run py.test
-sudo docker run -it --rm --link broker:broker -v '/var/run/docker.sock:/var/run/docker.sock' --name smr registry.sonata-nfv.eu:5000/specificmanagerregistry py.test -v
+sudo docker run -it --rm --link test.broker:broker -v '/var/run/docker.sock:/var/run/docker.sock' --name smr registry.sonata-nfv.eu:5000/specificmanagerregistry py.test -v
 
 echo "done."
