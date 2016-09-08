59d7e5c8229ee2d9712f2c899a521d1c5af07a5e
Hadi <hadi_k3r_2004@yahoo.com>
2016-09-08 13:16:25 +0200

    rerun

--- test/test_plugin-son-mano-smr.sh
+++ test/test_plugin-son-mano-smr.sh
@@ -39,7 +39,7 @@
 #
 
 # setup cleanup mechanism
-trap "set +e; docker rm -fv test.broker; docker rm -fv test.mongo; docker rm -fv test.pluginmanager; docker rm -fv test.smr; docker rm -fv ssm1; docker rm -fv ssm1_new" INT TERM EXIT
+trap "set +e; docker rm -fv test.broker; docker rm -fv test.mongo; docker rm -fv test.pluginmanager; docker rm -fv test.smr; docker rm -fv ssm1; docker rm -fv ssm2" INT TERM EXIT
 
 # ensure cleanup
 set +e
