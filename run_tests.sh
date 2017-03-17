5004ef552763c0de588383e5e592246c6e0b9ea9
tsoenen <thomas.soenen@intec.ugent.be>
2017-03-17 17:20:02 +0100

    The required containers to run this test script are now being build correctly.

--- run_tests.sh
+++ run_tests.sh
@@ -41,6 +41,8 @@ docker build -t registry.sonata-nfv.eu:5000/pluginmanager -f son-mano-pluginmana
 docker build -t registry.sonata-nfv.eu:5000/testplugin -f plugins/son-mano-test-plugin/Dockerfile .
 docker build -t registry.sonata-nfv.eu:5000/servicelifecyclemanagement -f plugins/son-mano-service-lifecycle-management/Dockerfile .
 #docker build -t registry.sonata-nfv.eu:5000/functionlifecyclemanagement -f plugins/son-mano-function-lifecycle-management/Dockerfile .
-#docker build -t registry.sonata-nfv.eu:5000/placementexecutive -f plugins/son-mano-placement/Dockerfile .
+docker build -t registry.sonata-nfv.eu:5000/placementexecutive -f plugins/son-mano-placement-executive/Dockerfile .
+docker build -t registry.sonata-nfv.eu:5000/scalingexecutive -f plugins/son-mano-scaling-executive/Dockerfile .
+docker build -t registry.sonata-nfv.eu:5000/specificmanagerregistry -f son-mano-specificmanager/son-mano-specific-manager-registry/Dockerfile .
 
 for i in `find . -name test_*.sh -type f`; do $i; if [ $? -ne 0 ]; then exit 1; fi; done
