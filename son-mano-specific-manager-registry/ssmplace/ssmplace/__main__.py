1c19ca7dda6f53a1046aa90c42eb3de61bfc8bc8
tsoenen <thomas.soenen@intec.ugent.be>
2017-03-17 10:48:11 +0100

    Deleting files that should not have been committed.

--- son-mano-specific-manager-registry/ssmplace/ssmplace/__main__.py
+++
@@ -1,8 +0,0 @@
-from ssmplace import ssmplace
-
-
-def main():
-    ssmplace.main()
-
-if __name__ == '__main__':
-    main()
--- son-mano-specific-manager-registry/ssmtask/ssmtask/__main__.py
+++
@@ -1,8 +0,0 @@
-from ssmtask import ssmtask
-
-
-def main():
-    ssmtask.main()
-
-if __name__ == '__main__':
-    main()
