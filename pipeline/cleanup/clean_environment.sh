64fc6df9efc3a7f0660ab6958aefc1482bd81381
root <thomas.soenen@ugent.be>
2018-03-03 11:35:14 +0100

    Ensuring that all stages of the clean-up are executed.

--- pipeline/cleanup/clean_environment.sh
+++ pipeline/cleanup/clean_environment.sh
@@ -7,9 +7,9 @@ echo "Clean dependencies"
 
 docker ps -a
 
-docker rm -fv $(docker ps -a -q -f "network=son-mano-unittests")
+docker rm -fv $(docker ps -a -q -f "network=son-mano-unittests") || true
 
-docker network rm son-mano-unittests
+docker network rm son-mano-unittests || true
 
 docker ps -a
 docker network ls
