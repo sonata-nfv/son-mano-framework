8e4a131319434e4b25ae67a48771931420af3950
Thomas Soenen <thomas.soenen@ugent.be>
2018-03-21 11:11:59 +0100

    Bugfix: hard copying dictionary is required

--- plugins/son-mano-service-lifecycle-management/son_mano_slm/slm.py
+++ plugins/son-mano-service-lifecycle-management/son_mano_slm/slm.py
@@ -751,7 +751,7 @@ class ServiceLifecycleManager(ManoBasePlugin):
             if content['workflow'] == 'migrate':
                 new_serv_id = str(uuid.uuid4())
                 self.services[new_serv_id] = {}
-                self.services[new_serv_id] = self.services[serv_id]
+                self.services[new_serv_id] = self.services[serv_id].copy()
                 self.services[new_serv_id]['old_serv_id'] = serv_id
                 self.migrate_workflow(new_serv_id, content['data'])
             # TODO: add additional workflows
